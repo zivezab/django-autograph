b6aca1f0cba95d47f52a0797a22f34dc100aa957
Zive <zive.lai@gmail.com>
2013-06-08 19:03:26 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-from distutils.core import setup
+import os
+
+from setuptools import setup, find_packages
 from distutils.core import Command
 
 
@@ -23,12 +25,28 @@ class TestCommand(Command):
 setup(
     name='autograph',
     version='1.0.0',
-    packages=['autograph'],
     author='Zive Lai',
     author_email='zive.lai@gmail.com',
+    packages=find_packages(),
     url='https://github.com/zivezab/django-autograph/',
     description='A Django application for managing signature/autograph data.',
-    long_description=open("README.md").read(),
+    long_description=open(os.path.join(os.getcwdu(), 'README.md')).read(),
     install_requires = ['django >= 1.4.5', 'south >= 0.8.1', 'pil >= 1.1.7'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Topic :: Communications :: Email',
+        'Topic :: Office/Business',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
     cmdclass={'test': TestCommand},
 )
